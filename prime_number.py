fa4d1a91eaa9015af025d26336f279a75221be16
nasum121 <nasum.sudharshan@gmail.com>
2018-07-06 00:03:30 -0700

    Create prime_number.py

---
+++ prime_number.py
@@ -0,0 +1,11 @@
+N = int(input("enter a number: "))
+           
+if N > 1:   
+   for i in range(2,N):
+       if (N % i) == 0:
+           print("no")
+           break
+   else:
+       print("yes")
+else:
+   print("no")
