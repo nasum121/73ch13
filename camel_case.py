0994d56dc76ea8a76b088f26ccbae8409c4efa8c
nasum121 <nasum.sudharshan@gmail.com>
2018-07-08 21:51:30 -0700

    Create camel_case.py

---
+++ camel_case.py
@@ -0,0 +1,14 @@
+input = input("Enter a string \n").lower() 
+words = input.split(' ')
+
+capitalized = []
+
+for word in words:
+    final_words = word[0].upper() + word[1:]
+    
+    capitalized.append(final_words) 
+output = ' '.join(capitalized)
+print (output) 
+
+
+
