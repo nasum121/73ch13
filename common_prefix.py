2d3353594eddb5cff051845ee76fdd89a6556ab2
nasum121 <nasum.sudharshan@gmail.com>
2018-07-13 00:25:00 -0700

    Update common_prefix.py

--- common_prefix.py
+++ common_prefix.py
@@ -32,7 +32,9 @@ if n==3:
     count=min(a,b,c)
     for i in range(count):
         if s1[i]==s2[i]==s3[i]:
-            new_word.append(s1[i])    
+            new_word.append(s1[i])
+
+        
     final="".join(new_word)
     print(final)
 if n==4:
@@ -54,5 +56,57 @@ if n==4:
     for i in range(count):
         if s1[i]==s2[i]==s3[i]==s4[i]:
             new_word.append(s[i])
+    final="".join(new_word)
+    print(final)
+if n==5:
+    string1=input()
+    string2=input()
+    string3=input()
+    string4=input()
+    string5=input()
+    s1=list(string1)
+    s2=list(string2)
+    s3=list(string3)
+    s4=list(string4)
+    s5=list(string5)
+    new_word=[]
+    
+    a=len(s1)
+    b=len(s2)
+    c=len(s3)
+    d=len(s3)
+    e=len(s4)
+    count=min(a,b,c,d,e)
+    for i in range(count):
+        if s1[i]==s2[i]==s3[i]==s4[i]==s5[i]:
+            new_word.append(s[i])
+    final="".join(new_word)
+    print(final)
+if n==6:
+    string1=input()
+    string2=input()
+    string3=input()
+    string4=input()
+    string5=input()
+    string6=input()
+    s1=list(string1)
+    s2=list(string2)
+    s3=list(string3)
+    s4=list(string4)
+    s5=list(string5)
+    s6=list(string6)
+    new_word=[]
+    
+    a=len(s1)
+    b=len(s2)
+    c=len(s3)
+    d=len(s3)
+    e=len(s4)
+    f=len(s5)
+    
+    count=min(a,b,c,d,e,f)
+    for i in range(count):
+        if s1[i]==s2[i]==s3[i]==s4[i]==s5[i]==s5[i]:
+            new_word.append(s1[i])
     final="".join(new_word)
     print(final)
