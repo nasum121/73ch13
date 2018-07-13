2e329a1047268d23a042573a3ffe1f40781080fb
nasum121 <nasum.sudharshan@gmail.com>
2018-07-12 23:22:22 -0700

    Update removing vowels and printing string in rev order.py

--- removing vowels and printing string in rev order.py
+++ removing vowels and printing string in rev order.py
@@ -1,9 +1,10 @@
-s=input()
+n,s=input().split()
+n=int(n)
 s=list(s)
 new=[]
 vowels='aeiouAEIOU'
 vowels=list(vowels)
-for i in range(len(s)):
+for i in range(n):
     if s[i] not in vowels:
         new.append(s[i])
                
