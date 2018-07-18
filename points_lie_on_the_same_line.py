0b8a4296c69d1090bad8c44e75164b2d7fc4e41a
nasum121 <nasum.sudharshan@gmail.com>
2018-07-18 02:59:21 -0700

    Update and rename points_lie_on_the_same_line_or_not.py to points_lie_on_the_same_line.py

--- points_lie_on_the_same_line.py
+++ points_lie_on_the_same_line.py
@@ -2,10 +2,10 @@ l1=input().split()
 l2=input().split()
 l3=input().split()
 a=l1[0]
+b=l1[1]
 if l1[0]==a and l2[0]==a and l3[0]==a:
     print("yes")
-b=l1[1]
-if l1[1]==b and l2[1]==b and l3[1]==b:
+elif l1[1]==b and l2[1]==b and l3[1]==b:
     print("yes")
 else:
     print("no")
